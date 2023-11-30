ecf0d95b4d823c5f5310d5700ae082518e23dd6c
Henzhc <henryzhchen02@gmail.com>
2023-12-01 12:58:10 +1300

    implemented paralax effect on background

---
+++ website.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const trippaForm = document.getElementById("trippa-content");
+    trippaForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        showTrippaForm();
+    });
+}
+
+showTrippaForm() {
+    
+}
